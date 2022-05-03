fa5261af219704c937157230d724ef81b0d338a1
rsataev <rusataev@gmail.com>
2022-05-03 16:27:33 -0400

    Version v2

---
+++ v2/arithmetic/arithmetic.go
@@ -0,0 +1,23 @@
+package arithmetic
+
+// Checks if a number is prime or not
+func IsPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func Factorial(n int) int {
+	var f int = 1
+	for i := 2; i <= n; i++ {
+		f *= i
+	}
+	return f
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/rsataev/gomath/v2
+
+go 1.17
